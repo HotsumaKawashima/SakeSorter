5dc051599b9f79f2ed3f7e9654999cf239bf338b
hotsuma kawashima <hkawashimajob1992@gmail.com>
2020-05-24 13:00:39 -0700

    add style

--- src/components/app/component.js
+++ src/components/app/component.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import './style.css';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+
 export default function App() {
   return (
-    <img src='/images/villagers/Admiral.png' />
+    <React.Fragment>
+      <CssBaseline />
+      <Box height='100%' display='flex' justifyContent='center' alignItems='center'>
+        <Box>
+          <img src='/images/villagers/Admiral.png' />
+        </Box>
+      </Box>
+    </React.Fragment>
   )
 }
