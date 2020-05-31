d02c698ea766a7e583545bf3600b522d407bfc55
hotsuma kawashima <hkawashimajob1992@gmail.com>
2020-05-31 15:38:04 -0700

    delete style.css

--- src/components/app/component.js
+++ src/components/app/component.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
-import './style.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Box from '@material-ui/core/Box';
 
 import Router from '../router';
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        html: {
+          height: '100%',
+        },
+        body: {
+          height: '100%',
+          margin: '0',
+          backgroundColor: '#FFF',
+        },
+        '#root': {
+          height: '100%',
+        }
+      },
+    },
+  },
+});
+
 export default function App() {
   return (
-    <React.Fragment>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router />
-    </React.Fragment>
+    </ThemeProvider>
   )
 }
