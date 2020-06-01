1fe6a38e1a69e8674efdadd34f8be87ffb5e7c57
hotsuma kawashima <hkawashimajob1992@gmail.com>
2020-05-31 17:40:34 -0700

    add sake image

--- src/components/frontPage/component.js
+++ src/components/frontPage/component.js
@@ -4,23 +4,42 @@ import { withStyles } from '@material-ui/core/styles';
 
 import Box    from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
 
-const StyledButton = withStyles({
+const StartButton = withStyles({
   root: {
     border: '1px solid #eee',
-    color: '#000',
+    color:  '#000',
   },
   label: {
     textTransform: 'capitalize',
   },
 })(Button);
 
+const SakeImage = withStyles({
+  root: {
+    border: '1px solid #eee',
+    padding: '10px',
+    width:  '90px',
+    height: '90px',
+    position: 'absolute',
+  }
+})(Avatar);
+
 export default function SortPage(props) {
   return (
     <Box height='100%' display='flex' justifyContent='center' alignItems='center'>
-      <Box>
-        <Box height={50} display='flex' justifyContent='center' alignItems='center'>
-          <StyledButton variant='outlined'>FIND YOUR SAKE</StyledButton>
+      <Box style={{ position: 'relative' }}>
+        <SakeImage src='./images/sakes/aiyama720_800.jpg' style={{ left:   '30px', top: '-175px' }} />
+        <SakeImage src='./images/sakes/aiyama720_800.jpg' style={{ left:  '145px', top: '-135px' }} />
+        <SakeImage src='./images/sakes/aiyama720_800.jpg' style={{ left:  '-85px', top: '-135px' }} />
+        <SakeImage src='./images/sakes/aiyama720_800.jpg' style={{ left:  '195px', top:  '-30px' }} />
+        <SakeImage src='./images/sakes/aiyama720_800.jpg' style={{ left: '-125px', top:  '-30px' }} />
+        <SakeImage src='./images/sakes/aiyama720_800.jpg' style={{ left:  '-85px', top:   '85px' }} />
+        <SakeImage src='./images/sakes/aiyama720_800.jpg' style={{ left:  '145px', top:   '85px' }} />
+        <SakeImage src='./images/sakes/aiyama720_800.jpg' style={{ left:   '30px', top:  '125px' }} />
+        <Box display='flex' justifyContent='center' alignItems='center'>
+          <StartButton variant='outlined'>FIND YOUR SAKE</StartButton>
         </Box>
       </Box>
     </Box>
